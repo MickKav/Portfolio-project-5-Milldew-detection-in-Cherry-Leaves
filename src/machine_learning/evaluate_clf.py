baea1100ef3e285e1e20060fadd1ddc8fb125c9a
MickKav <mkavanagh187@gmail.com>
2024-01-21 15:54:04 +0000

    Refer to Walkthrough project for src files

--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -1,6 +1,8 @@
 import streamlit as st
 from src.data_management import load_pkl_file
 
-
 def load_test_evaluation(version):
+    """
+    Load test evaluation data from a pickled file
+    """
     return load_pkl_file(f'outputs/{version}/evaluation.pkl')
