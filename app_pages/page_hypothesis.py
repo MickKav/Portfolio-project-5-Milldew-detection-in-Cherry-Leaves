6c3552485eac339964d75f1943693550ebadcd6e
MickKav <mkavanagh187@gmail.com>
2024-01-21 19:21:15 +0000

    Build hypothesis page and displayed effected mildew data in dashboard

--- app_pages/page_hypothesis.py
+++ app_pages/page_hypothesis.py
@@ -18,15 +18,12 @@ def hypothesis_validation():
     st.write("To validate this hypothesis, we will analyze the growth characteristics of mildewed leaves.")
     st.write("Symptoms to look for include curled leaves, smaller and pale leaves, distorted growth, and the presence of a whitish fungus.")
 
-    # Hypothesis 2
-    st.subheader("Hypothesis 2:")
-    st.write("We hypothesize that the growth of sour cherry trees is significantly reduced by the presence of the mildew disease.")
-    
-    # Validation for Hypothesis 2
-    st.subheader("Validation:")
-    st.write("To validate this hypothesis, we will analyze the growth patterns of infected trees compared to healthy ones.")
-    st.write("Measurements will include shoot length, leaf size, and overall tree development.")
-    # Example: Display charts or graphs comparing the growth metrics of infected and healthy trees
+    with st.beta_expander("View Mildewed Leaves Examples"):
+        # You can add multiple images here to showcase examples of mildewed leaves
+        mildewed_leaf_image_1 = "notebooks/inputs/cherry_leaves_dataset/cherry-leaves/train/powdery_mildew/0ceb54ca-c9c1-48fb-9b8c-eb8afaefc378___FREC_Pwd.M 4698_flipLR.JPG"
+        mildewed_leaf_image_2 = "notebooks/inputs/cherry_leaves_dataset/cherry-leaves/train/powdery_mildew/0c22dfef-7413-4f46-b949-903d238678a3___FREC_Pwd.M 5094_flipLR.JPG"
+        mildewed_leaf_image_3 = "notebooks/inputs/cherry_leaves_dataset/cherry-leaves/train/powdery_mildew/00e0a4ab-ecbd-4560-a71c-b19d86bb087c___FREC_Pwd.M 4917_flipLR.JPG"
 
-# Uncomment the line below if you want to test this page individually
-# hypothesis_validation()
+        st.image([mildewed_leaf_image_1, mildewed_leaf_image_2, mildewed_leaf_image_3], caption=["Mildewed Leaf 1", "Mildewed Leaf 2", "Mildewed Leaf 3"])
+
+    st.write("Average Image, Variability Image and Difference between Averages studies did not reveal any clear pattern to differentiate one from another.")
