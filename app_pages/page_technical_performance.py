3736cfe7b5090d977add3c5715562e223105f924
MickKav <mkavanagh187@gmail.com>
2024-01-21 21:21:51 +0000

    Code ip technical performance and leave ready for styling

--- app_pages/page_technical_performance.py
+++ app_pages/page_technical_performance.py
@@ -1,4 +1,5 @@
 import streamlit as st
+import pandas as pd
 import matplotlib.pyplot as plt
 from src.machine_learning.evaluate_clf import load_test_evaluation
 
@@ -15,13 +16,26 @@ def technical_performance_body():
     st.image(labels_distribution, caption='Labels Distribution on Train, Validation, and Test Sets')
     st.write("---")
 
-    # Example: Display confusion matrix, ROC curve, or other relevant visualizations
-    st.header("Visualizations")
-    st.write("Include visualizations that showcase the model's performance.")
+    # Model History - Training Accuracy and Losses
+    st.header("Model History")
+    col1, col2 = st.beta_columns(2)
+    with col1:
+        model_acc = plt.imread(f"notebooks/outputs/{version}/model_training_acc.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"notebooks/outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
 
-    # Additional details, insights, or analysis related to technical performance
-    st.header("Analysis and Insights")
-    st.write("Provide any additional insights or analysis related to the technical performance of the model.")
+    # Generalized Performance on Test Set - Evaluation Metrics
+    st.header("Generalized Performance on Test Set")
+    evaluation_data = {
+        'Metric': ['Loss', 'Accuracy'],
+        'Value': [0.0355, 0.9976]
+    }
+
+    evaluation_df = pd.DataFrame(evaluation_data).set_index('Metric')
+    st.dataframe(evaluation_df)
 
     # Conclusion and recommendations
     st.header("Conclusion and Recommendations")
--- src/machine_learning/evaluate_clf.py
+++ src/machine_learning/evaluate_clf.py
@@ -5,4 +5,4 @@ def load_test_evaluation(version):
     """
     Load test evaluation data from a pickled file
     """
-    return load_pkl_file(f'outputs/{version}/evaluation.pkl')
+    return load_pkl_file(f'notebooks/outputs/{version}/evaluation.pkl')
